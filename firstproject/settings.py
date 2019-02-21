dd895971596288fc8d67f9d6817e8a065be6b3f3
Junghoon <bjh5230@gmail.com>
2019-02-21 11:40:37 +0900

    allowed_host all

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '8*!3j6&n_mg%z&hu@u1eppoj)!tr8s
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
